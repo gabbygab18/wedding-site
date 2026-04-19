372bf152e0594a07f3b418e93244586c34f075da
gabbygab <cpe.villanueva.gabrielandrei@gmail.com>
2026-04-19 15:17:20 +0800

    Fix log channel and migration order

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 chmod -R 777 /var/www/storage /var/www/bootstrap/cache
+php artisan config:clear
+php artisan migrate --force 2>&1
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --force
 php-fpm -D
 nginx -g "daemon off;"
