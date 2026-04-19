d2952171dc1c6e356e8013f61af4dfc69f4ca134
gabbygab <cpe.villanueva.gabrielandrei@gmail.com>
2026-04-19 18:22:42 +0800

    Clear view cache on deploy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,7 @@
 chmod -R 777 /var/www/storage /var/www/bootstrap/cache
 php artisan config:clear
 php artisan cache:clear
+php artisan view:clear
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
