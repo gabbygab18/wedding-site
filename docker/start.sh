ad8ac1efd205354f714b1cf468409a395244d5c3
gabbygab <cpe.villanueva.gabrielandrei@gmail.com>
2026-04-19 15:30:28 +0800

    save

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 chmod -R 777 /var/www/storage /var/www/bootstrap/cache
 php artisan config:clear
-php artisan migrate --force 2>&1
+php artisan cache:clear
+php artisan migrate --force
+php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
